5b3251aa06a50aa0ec9d9725e1a81688fd7136a9
Steve Nguyen <steve@coderomp.com>
2019-02-28 08:49:47 +0000

    updated source data and renamed file.

--- download_data.py
+++ download_data.py
@@ -3,6 +3,7 @@ import datetime
 
 def download():
     attempts = 0
+    url2009 = 'https://data.treasury.gov/feed.svc/DailyTreasuryYieldCurveRateData?$filter=year(NEW_DATE)%20eq%202008'
     url2018 = 'https://data.treasury.gov/feed.svc/DailyTreasuryYieldCurveRateData?$filter=year(NEW_DATE)%20eq%202018'
     url2019 = 'https://data.treasury.gov/feed.svc/DailyTreasuryYieldCurveRateData?$filter=year(NEW_DATE)%20eq%202019'
     current_date = datetime.datetime.now().strftime("%m-%d-%y")
